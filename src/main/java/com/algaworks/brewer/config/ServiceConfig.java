292dbffd247607d93d8af47ff3eb5a2f1bdeafe2
Acer <onoffjunior@gmail.com>
2022-08-26 07:33:59 -0300

    11.1 Validando se existe um estilo persistido

--- src/main/java/com/algaworks/brewer/config/ServiceConfig.java
+++ src/main/java/com/algaworks/brewer/config/ServiceConfig.java
@@ -4,9 +4,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.algaworks.brewer.service.CadastroCervejaService;
+import com.algaworks.brewer.service.CadastroEstiloService;
 
 @Configuration
 @ComponentScan(basePackageClasses = CadastroCervejaService.class)
+@ComponentScan(basePackageClasses = CadastroEstiloService.class)
 public class ServiceConfig {
 
 }
