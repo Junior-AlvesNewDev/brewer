0b1f0afffb7dcec23e6ceab4ad46a3a72036c936
Acer <onoffjunior@gmail.com>
2022-09-19 07:27:59 -0300

    11.3 Tratando exceções com ExceptionHandler

--- src/main/java/com/algaworks/brewer/controller/EstilosController.java
+++ src/main/java/com/algaworks/brewer/controller/EstilosController.java
@@ -52,13 +52,10 @@ public class EstilosController {
 		if (result.hasErrors()){
 			return ResponseEntity.badRequest().body(result.getFieldError("nome").getDefaultMessage());
 		}
-		try {
-			estilo = cadastroEstiloService.salvar(estilo);
-		} catch (NomeEstiloJaCadastradoException e){
-			return ResponseEntity.badRequest().body(e.getMessage());
-		}
-		
+			
+		estilo = cadastroEstiloService.salvar(estilo);	
 		return ResponseEntity.ok(estilo);
+		
 	}
 
 }
