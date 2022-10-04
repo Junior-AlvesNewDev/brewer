6e44b537e4861ef0c963b5b8d4f6e59237007b24
Acer <onoffjunior@gmail.com>
2022-10-04 08:01:01 -0300

    14.5 Salvando o nome da foto da cerveja

--- src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -76,6 +76,11 @@ public class Cerveja {
 	@JoinColumn(name = "codigo_estilo")
 	private Estilo estilo;
 	
+	private String foto;
+	
+	@Column(name = "content_type")
+	private String contentType;
+	
 	@PrePersist 
 	@PreUpdate
 	private void prePersistUpdate(){
@@ -148,6 +153,23 @@ public class Cerveja {
 	public void setEstilo(Estilo estilo) {
 		this.estilo = estilo;
 	}
+	
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getContentType() {
+		return contentType;
+	}
+
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
